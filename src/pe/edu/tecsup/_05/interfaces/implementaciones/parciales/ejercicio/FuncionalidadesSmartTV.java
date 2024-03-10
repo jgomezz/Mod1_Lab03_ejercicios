495bc831d7c606996eb019a7dbc1aeb80a499092
Your Name <you@example.com>
2024-03-10 12:34:59 -0500

    Update

---
+++ src/pe/edu/tecsup/_05/interfaces/implementaciones/parciales/ejercicio/FuncionalidadesSmartTV.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup._05.interfaces.implementaciones.parciales.ejercicio;
+
+public interface FuncionalidadesSmartTV {
+
+    void controlarPorVoz();
+    
+    void accederAInternet();
+    
+    void ejecutarAplicaciones();
+    
+    void programarAlarmas();
+
+}
---
+++ src/pe/edu/tecsup/_05/interfaces/implementaciones/parciales/ejercicio/SamsungSmartTV.java
@@ -0,0 +1,30 @@
+package pe.edu.tecsup._05.interfaces.implementaciones.parciales.ejercicio;
+
+public class SamsungSmartTV 
+implements FuncionalidadesSmartTV {
+
+    @Override
+    public void controlarPorVoz() {
+        // TODO Auto-generated method stub
+         System.out.println("Proceso aun EN desarrollo");
+    }
+
+    @Override
+    public void accederAInternet() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void ejecutarAplicaciones() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void programarAlarmas() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
